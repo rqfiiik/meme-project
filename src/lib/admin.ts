dfcd310cfea106b2da5fdb4bb3329ee9c46120c3
Rafik Lakehal <rqfik.lakehal@gmail.com>
2026-01-26 19:03:24 +0400

    feat: introduce `isUserAdmin` utility to determine admin status based on email, role, and wallet address.

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,3 +1,5 @@
+import { User } from "@prisma/client";
+
 export function isUserAdmin(user: Partial<User> | null, walletAddress?: string): boolean {
     const HARDCODED_ADMIN = "rqfik.lakehal@gmail.com";
 
